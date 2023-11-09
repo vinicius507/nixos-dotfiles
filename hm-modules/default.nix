75be59a24cd9920935f1cac664a75ac186da0f86
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-09 13:53:17 -0300

    feat(cli): adds packages which do not require hm modules

--- hm-modules/default.nix
+++ hm-modules/default.nix
@@ -4,12 +4,19 @@
     home.homeDirectory = "/home/vini";
     home.stateVersion = "23.05";
   };
-  cli = {...}: {
+  cli = {pkgs, ...}: {
     imports = [
       ./cli/direnv.nix
       ./cli/fish.nix
       ./cli/starship.nix
       ./cli/git.nix
+    ];
+
+    home.packages = with pkgs; [
+      fd
+      ripgrep
+      tldr
+      tree
     ];
   };
 }
