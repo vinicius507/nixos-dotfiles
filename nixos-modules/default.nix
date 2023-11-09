0b9acc83ae0da2e186f5bfadc08d7541796f4511
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-09 16:39:42 -0300

    chore: enables graphical user interface in vmVariant

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -3,8 +3,12 @@
   vm = {
     virtualisation.vmVariant = {
       virtualisation.cores = 2;
-      virtualisation.graphics = false;
       virtualisation.memorySize = 2048;
+      virtualisation.qemu.options = [
+        "-device virtio-vga-gl"
+        "-display sdl,gl=on,show-cursor=off"
+        "-audio pa,model=hda"
+      ];
     };
   };
 }
