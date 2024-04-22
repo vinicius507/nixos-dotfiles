22cdcf4d0472bb6a209f683796caadaba5bf1a5c
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-04-22 17:24:48 -0300

    feat: Adds nvim-cmp sources

--- hm-modules/neovim/config/lua/plugins/coding.lua
+++ hm-modules/neovim/config/lua/plugins/coding.lua
@@ -31,6 +31,8 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
+			"f3fora/cmp-spell",
+			"hrsh7th/cmp-calc",
 			"hrsh7th/cmp-emoji",
 			"onsails/lspkind.nvim",
 		},
@@ -38,9 +40,35 @@ return {
 		opts = function(_, opts)
 			local cmp = require("cmp")
 
-			opts.sources = vim.list_extend(opts.sources, { { name = "emoji" } })
+			opts.sources = vim.list_extend(opts.sources, {
+				{ name = "calc" },
+				{ name = "emoji" },
+				{
+					name = "spell",
+					option = {
+						keep_all_entries = false,
+						enable_in_context = function()
+							return require("cmp.config.context").in_treesitter_capture("spell")
+						end,
+					},
+				},
+			})
 			opts.formatting.fields = { cmp.ItemField.Kind, cmp.ItemField.Abbr, cmp.ItemField.Menu }
-			opts.formatting.format = require("lspkind").cmp_format({ mode = "symbol", maxwidth = 50 })
+			opts.formatting.format = require("lspkind").cmp_format({
+				mode = "symbol",
+				maxwidth = 50,
+				symbol_map = {
+					Calc = "󰃬",
+				},
+				---@param entry cmp.Entry
+				---@param vim_item vim.CompletedItem
+				before = function(entry, vim_item)
+					if entry.source.name == "calc" then
+						vim_item.kind = "Calc"
+					end
+					return vim_item
+				end,
+			})
 			opts.view = {
 				entries = {
 					name = "custom",
