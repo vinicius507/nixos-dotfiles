acfa6760cb3a6f339cac9dbcff911b6674e0c88a
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-04-23 12:39:57 -0300

    fix(nvim): Removes duplicate keybindings

--- hm-modules/neovim/config/lua/config/keymaps.lua
+++ hm-modules/neovim/config/lua/config/keymaps.lua
@@ -6,16 +6,6 @@ vim.keymap.set("n", "N", "Nzzzv", { silent = true })
 -- Better cursor positioning while joining lines
 vim.keymap.set("n", "J", "mzJ`z", { silent = true })
 
--- Undo breakpoints
-vim.keymap.set("i", ",", ",<C-g>u", { silent = true })
-vim.keymap.set("i", ".", ".<C-g>u", { silent = true })
+-- Extra undo breakpoints
 vim.keymap.set("i", "!", "!<C-g>u", { silent = true })
 vim.keymap.set("i", "?", "?<C-g>u", { silent = true })
-
--- Moving Text
-vim.keymap.set("v", ">", ">gv", { silent = true })
-vim.keymap.set("v", "<", "<gv", { silent = true })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
-vim.keymap.set("i", "<C-j>", "<esc>:m .+1<CR>==a", { silent = true })
-vim.keymap.set("i", "<C-k>", "<esc>:m .-2<CR>==a", { silent = true })
