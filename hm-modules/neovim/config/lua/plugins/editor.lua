c27aa6783d8537faf7f489b053fb2dc292f7ad6d
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-04-30 02:46:59 -0300

    feat: Adds icons to harpoon

--- hm-modules/neovim/config/lua/plugins/editor.lua
+++ hm-modules/neovim/config/lua/plugins/editor.lua
@@ -53,6 +53,19 @@ return {
 				desc = "Harpoon Quick Menu",
 			},
 		},
+		---@type HarpoonPartialConfig
+		opts = {
+			default = {
+				display = function(list_item)
+					local filename = vim.fn.fnamemodify(list_item.value, ":t")
+					local extension = vim.fn.fnamemodify(list_item.value, ":e")
+					local icon = require("nvim-web-devicons").get_icon(filename, extension, {
+						default = true,
+					})
+					return string.format("%s %s", icon, list_item.value)
+				end,
+			},
+		},
 	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
