5022d5ccf3c31355c0a68bcd673af52241769c71
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-05-04 18:14:25 -0300

    feat: adds common languages in default config

--- hm-modules/neovim/config/lua/plugins/langs.lua
+++ hm-modules/neovim/config/lua/plugins/langs.lua
@@ -1,7 +1,14 @@
 ---@type LazySpec
 return {
+	{ import = "lazyvim.plugins.extras.lang.clangd" },
 	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.go" },
 	{ import = "lazyvim.plugins.extras.lang.json" },
 	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	{ import = "lazyvim.plugins.extras.lang.tailwind" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.formatting.prettier" },
+	{ import = "lazyvim.plugins.extras.linting.eslint" },
 }
