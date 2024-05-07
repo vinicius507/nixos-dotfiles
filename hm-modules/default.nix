4626bd425f6f6692c98ba3d9f7859f32e71643a9
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-05-07 12:54:48 -0300

    fix: Use tmpfs for Downloads folder

--- hm-modules/default.nix
+++ hm-modules/default.nix
@@ -9,6 +9,10 @@
       enable = true;
       pinentryFlavor = "curses";
     };
+    systemd.user.tmpfiles.rules = [
+      "d %t/downloads 0700 - - - -"
+      "L %h/Downloads - - - - %t/downloads"
+    ];
   };
   cli = {pkgs, ...}: {
     imports = [
