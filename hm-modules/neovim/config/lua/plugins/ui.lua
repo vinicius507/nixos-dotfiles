eada3ab1a6926fb21e488d463a2ba19eec6ba543
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-05-10 13:22:44 -0300

    feat(neovim): Removes unwanted sections from lualine

--- hm-modules/neovim/config/lua/plugins/ui.lua
+++ hm-modules/neovim/config/lua/plugins/ui.lua
@@ -14,6 +14,18 @@ return {
 		},
 	},
 	{
+		"nvim-lualine/lualine.nvim",
+		opts = function(_, opts)
+			return vim.tbl_deep_extend("force", opts, {
+				options = { section_separators = "", component_separators = "" },
+				sections = {
+					lualine_y = {},
+					lualine_z = {},
+				},
+			})
+		end,
+	},
+	{
 		"folke/noice.nvim",
 		opts = function(_, opts)
 			opts.presets.lsp_doc_border = true
