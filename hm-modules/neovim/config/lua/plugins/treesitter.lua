4c185554635e4832d72780fb48e9832681dc9d7c
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-05-21 16:23:44 -0300

    fix(neovim): Restructures nvim-treesitter config

--- hm-modules/neovim/config/lua/plugins/treesitter.lua
+++ hm-modules/neovim/config/lua/plugins/treesitter.lua
@@ -1,23 +1,24 @@
+---@type LazySpec
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		dependencies = { "windwp/nvim-ts-autotag" },
-		opts = {
-			auto_install = true,
-			autotag = {
-				enable = true,
-			},
-			ensure_installed = {
-				"doxygen",
-				"nix",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+			opts = {
+				max_lines = 5,
 			},
 		},
 	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		opts = {
-			max_lines = 5,
+	opts = {
+		auto_install = true,
+		autotag = {
+			enable = true,
+		},
+		ensure_installed = {
+			"doxygen",
+			"nix",
 		},
 	},
 }
