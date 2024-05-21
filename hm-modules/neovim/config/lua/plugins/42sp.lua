8e92f7c3815cb02334a561a71c4e2c9e1ea35665
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-05-21 02:16:49 -0300

    fix(neovim): Loads header42.nvim in C, C++ and make filetypes

--- hm-modules/neovim/config/lua/plugins/42sp.lua
+++ hm-modules/neovim/config/lua/plugins/42sp.lua
@@ -1,8 +1,10 @@
+---@type LazySpec
 return {
 	-- TODO: Add Norminette https://github.com/42school/norminette
 	{
 		"vinicius507/header42.nvim",
 		cmd = { "Stdheader" },
+		ft = { "c", "cpp", "make" },
 		opts = {
 			login = "vgoncalv",
 			email = "vgoncalv@student.42sp.org.br",
