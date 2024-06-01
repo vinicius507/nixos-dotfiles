2bcd5cc64efdd599488173d0db7f0850e2dc1f40
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-06-01 12:08:40 -0300

    fix(neovim): Removes trouble-v3 extra

--- hm-modules/neovim/config/lua/config/lazy.lua
+++ hm-modules/neovim/config/lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
 		{ import = "lazyvim.plugins.extras.editor.refactoring" },
-		{ import = "lazyvim.plugins.extras.editor.trouble-v3" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
