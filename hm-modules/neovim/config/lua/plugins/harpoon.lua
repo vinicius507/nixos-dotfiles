63629eae6b2c487ef3241924feb78784309b768c
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-06-01 12:24:34 -0300

    fix(neovim): Removes useless imports

--- hm-modules/neovim/config/lua/plugins/harpoon.lua
+++ hm-modules/neovim/config/lua/plugins/harpoon.lua
@@ -1,38 +1,35 @@
 return {
-	{ import = "lazyvim.plugins.extras.editor.harpoon2" },
-	{
-		"ThePrimeagen/harpoon",
-		keys = {
-			{ "<Leader>h", false },
-			{ "<Leader>H", false },
-			{
-				"<Leader>a",
-				function()
-					require("harpoon"):list():add()
-				end,
-				desc = "Harpoon file",
-			},
-			{
-				"<C-e>",
-				function()
-					local harpoon = require("harpoon")
-					harpoon.ui:toggle_quick_menu(harpoon:list())
-				end,
-				desc = "Harpoon Quick Menu",
-			},
+	"ThePrimeagen/harpoon",
+	keys = {
+		{ "<Leader>h", false },
+		{ "<Leader>H", false },
+		{
+			"<Leader>a",
+			function()
+				require("harpoon"):list():add()
+			end,
+			desc = "Harpoon file",
 		},
-		---@type HarpoonPartialConfig
-		opts = {
-			default = {
-				display = function(list_item)
-					local filename = vim.fn.fnamemodify(list_item.value, ":t")
-					local extension = vim.fn.fnamemodify(list_item.value, ":e")
-					local icon = require("nvim-web-devicons").get_icon(filename, extension, {
-						default = true,
-					})
-					return string.format("%s %s", icon, list_item.value)
-				end,
-			},
+		{
+			"<C-e>",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "Harpoon Quick Menu",
+		},
+	},
+	---@type HarpoonPartialConfig
+	opts = {
+		default = {
+			display = function(list_item)
+				local filename = vim.fn.fnamemodify(list_item.value, ":t")
+				local extension = vim.fn.fnamemodify(list_item.value, ":e")
+				local icon = require("nvim-web-devicons").get_icon(filename, extension, {
+					default = true,
+				})
+				return string.format("%s %s", icon, list_item.value)
+			end,
 		},
 	},
 }
--- hm-modules/neovim/config/lua/plugins/mini.lua
+++ hm-modules/neovim/config/lua/plugins/mini.lua
@@ -1,18 +1,14 @@
----@type LazySpec
 return {
-	{ import = "lazyvim.plugins.extras.coding.mini-surround" },
-	{
-		"echasnovski/mini.surround",
-		opts = {
-			mappings = {
-				add = "gsa",
-				delete = "gsd",
-				find = "gsf",
-				find_left = "gsF",
-				highlight = "gsh",
-				replace = "gsr",
-				update_n_lines = "gsn",
-			},
+	"echasnovski/mini.surround",
+	opts = {
+		mappings = {
+			add = "gsa",
+			delete = "gsd",
+			find = "gsf",
+			find_left = "gsF",
+			highlight = "gsh",
+			replace = "gsr",
+			update_n_lines = "gsn",
 		},
 	},
 }
