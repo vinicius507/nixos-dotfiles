773ac477c7daaa3ee9ae5dc909a90b74c47a3224
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-06-09 01:42:12 -0300

    feat(neovim): Updates lazydev.nvim library paths and conditions

--- hm-modules/neovim/config/lua/plugins/lazydev.lua
+++ hm-modules/neovim/config/lua/plugins/lazydev.lua
@@ -1,10 +1,16 @@
 return {
 	"folke/lazydev.nvim",
+	---@type lazydev.Config
 	opts = {
 		library = {
-			catppuccin = "catppuccin",
-			["lazy.nvim"] = "lazy.nvim",
-			["trouble.nvim"] = "trouble.nvim",
+			{
+				path = "catppuccin",
+				mods = { "catppuccin" },
+				words = { "CatppuccinOptions" },
+			},
+			{ path = "lazy.nvim", words = { "LazySpec" } },
+			{ path = "lazydev.nvim", words = { "lazydev%.Config" } },
+			{ path = "trouble.nvim", words = { "trouble%.Config" } },
 		},
 	},
 }
