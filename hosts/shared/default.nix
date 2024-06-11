53b09f41856baa92f2164f717d047f51416c6ffb
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-06-11 14:09:36 -0300

    fix: stylix.enable :)

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -125,6 +125,7 @@
   };
 
   stylix = {
+    enable = true;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     image = builtins.fetchurl {
