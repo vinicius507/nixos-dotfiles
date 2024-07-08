95998b49d9d5ee9a93ed359242353e91e137bed6
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-08 16:05:31 -0300

    chore(nvim): DRY with this lib

--- hm-modules/neovim/config/lua/plugins/toggleterm.lua
+++ hm-modules/neovim/config/lua/plugins/toggleterm.lua
@@ -1,3 +1,5 @@
+local term = require("lib.terminal")
+
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
@@ -14,17 +16,17 @@ return {
 		},
 		{ "<C-/>", [[<esc><cmd>ToggleTerm dir=git_dir name=workspace<cr>]], mode = "i", desc = "Toggle Terminal" },
 		{ "<Leader>ft", "<cmd>TermSelect<cr>", desc = "Find Terminal" },
-		{ "<Leader>gg", require("lib.terminal").lazygit, desc = "Lazygit" },
+		{ "<Leader>gg", term.lazygit, desc = "Lazygit" },
 		{
 			"<Leader>gf",
 			function()
 				local git_path = vim.api.nvim_buf_get_name(0)
-				require("lib.terminal").lazygit({ "-f", vim.trim(git_path) })
+				term.lazygit({ "-f", vim.trim(git_path) })
 			end,
 			desc = "Lazygit Current File History",
 		},
 		{ "<Leader>tr", "<cmd>ToggleTermSetName<cr>", desc = "Rename Terminal" },
-		{ "<Leader>tn", require("lib.terminal").new, desc = "New Terminal" },
+		{ "<Leader>tn", term.new, desc = "New Terminal" },
 	},
 	opts = {
 		open_mapping = false,
