fb43779d0de274ee40c52bb3c484fed4c14ba968
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-08 16:11:22 -0300

    feat(neeovim): Adds git-dev.nvim

---
+++ hm-modules/neovim/config/lua/plugins/git.lua
@@ -0,0 +1,32 @@
+local function gitdev_open()
+	local repo = vim.fn.input("Repository")
+
+	if repo ~= "" then
+		require("git-dev").open(repo)
+	end
+end
+
+return {
+	{
+		"moyiz/git-dev.nvim",
+		cmd = {
+			"GitDevOpen",
+			"GitDevToggleUI",
+			"GitDevRecents",
+			"GitDevCleanAll",
+		},
+		keys = {
+			{ "<leader>go", gitdev_open, desc = "Open a remote git repository" },
+		},
+		opts = {
+			cd_type = "tab",
+			opener = function(dir, _, selected_path)
+				vim.cmd.tabnew()
+				vim.cmd.Oil(dir)
+				if selected_path then
+					vim.cmd("edit " .. selected_path)
+				end
+			end,
+		},
+	},
+}
