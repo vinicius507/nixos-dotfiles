4c8f18c6565b6f617666845986d1b996f3f883d6
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-16 15:39:15 -0300

    feat(neovim): Adds FtHeader keybinding

--- hm-modules/neovim/config/lua/plugins/42sp.lua
+++ hm-modules/neovim/config/lua/plugins/42sp.lua
@@ -2,8 +2,17 @@
 return {
 	{
 		"vinicius507/ft_nvim",
+		ft = { "c", "cpp", "make" },
 		cmd = { "FtHeader", "Norme" },
-		ft = { "c", "cpp" },
+		keys = {
+			{
+				"<F2>",
+				"<cmd>FtHeader<CR>",
+				mode = "n",
+				desc = "Inserts/updates 42 header",
+				silent = true,
+			},
+		},
 		opts = {
 			header = {
 				username = "vgoncalv",
