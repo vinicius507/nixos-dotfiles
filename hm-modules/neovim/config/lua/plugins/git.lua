3a4d71463931e23988c23e742308d0fdceb8b6fd
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-08-01 14:36:36 -0300

    feat(neovim): Create autocmd to close fugitive with q

--- hm-modules/neovim/config/lua/plugins/git.lua
+++ hm-modules/neovim/config/lua/plugins/git.lua
@@ -11,6 +11,20 @@ return {
 		"tpope/vim-fugitive",
 		cmd = { "Git" },
 		ft = { "gitcommit", "gitrebase", "gitconfig" },
+		config = function()
+			vim.api.nvim_create_autocmd({ "FileType" }, {
+				group = vim.api.nvim_create_augroup("fugitive_close_with_q", { clear = true }),
+				pattern = { "fugitive" },
+				callback = function(ev)
+					vim.bo[ev.buf].buflisted = false
+					vim.keymap.set("n", "q", "<cmd>close<CR>", {
+						buffer = ev.buf,
+						silent = true,
+						desc = "Quit buffer",
+					})
+				end,
+			})
+		end,
 	},
 	{
 		"moyiz/git-dev.nvim",
