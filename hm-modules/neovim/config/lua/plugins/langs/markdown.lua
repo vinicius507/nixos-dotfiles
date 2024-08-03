96ce6d5dd6bb1002abb58369c22e9598145fad70
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-08-03 15:56:39 -0300

    feat(neovim): Adds markdown configuration

---
+++ hm-modules/neovim/config/lua/plugins/langs/markdown.lua
@@ -0,0 +1,14 @@
+return {
+	"markdown.nvim",
+	opts = {
+		code = {
+			width = "full",
+		},
+		heading = {
+			sign = true,
+			width = "block",
+			icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+			position = "inline",
+		},
+	},
+}
