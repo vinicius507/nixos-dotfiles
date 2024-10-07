2e794e2024091b22c3cf512f813867e08d92bafb
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-10-07 14:48:39 -0300

    feat(minipc): Changes ssh oprt

--- hosts/minipc/default.nix
+++ hosts/minipc/default.nix
@@ -41,14 +41,17 @@
 
   security.rtkit.enable = true;
 
-  services.pipewire = {
-    enable = true;
-    alsa = {
+  services = {
+    openssh.ports = [2222];
+    pipewire = {
       enable = true;
-      support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      jack.enable = true;
     };
-    pulse.enable = true;
-    jack.enable = true;
   };
 
   users = {
